f387542acf95f116f771a0a05827a487ba4ae1e0
riyandarmawan <riyandarmawan042@gmail.com>
2026-07-30 23:22:48 +0700

    Enhance login functionality and UI with NIM input, improved styles, and new color themes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,43 +5,49 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- NIM  -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="nim" :value="__('NIM')" />
+            <div class="relative">
+                <span class="icon-[tabler--id] text-zinc-400 absolute text-2xl top-1/2 left-2 -translate-y-1/2"></span>
+                <x-text-input id="nim" placeholder="Masukkan NIM anda" class="block mt-1 w-full" type="nim"
+                    name="nim" :value="old('nim')" required autofocus autocomplete="username" />
+            </div>
+            <x-input-error :messages="$errors->get('nim')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
+            <div class="relative">
+                <span
+                    class="icon-[material-symbols--lock-outline] text-zinc-400 absolute text-2xl top-1/2 left-2 -translate-y-1/2"></span>
+                <x-text-input id="password" placeholder="Masukkan password anda" class="block mt-1 w-full"
+                    type="password" name="password" required autocomplete="current-password" />
+            </div>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="block my-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                    name="remember">
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Ingat saya') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <x-primary-button class="block w-full text-center mb-4">
+            {{ __('Log in') }}
+        </x-primary-button>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+        @if (Route::has('password.request'))
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 block text-center"
+                href="{{ route('password.request') }}">
+                {{ __('Lupa password?') }}
+            </a>
+        @endif
     </form>
 </x-guest-layout>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,4 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled)
+    {{ $attributes->merge(['class' => 'p-2 ps-10  placeholder-zinc-400 rounded-md border  focus:border-primary-500 focus:ring-2 outline-none focus:ring-primary-500']) }}>
