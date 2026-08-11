d114dfd8b3b2412fb162ce128e727dee1b6ab53e
helmyeahh <helmyfa7@gmail.com>
2026-08-11 16:51:24 +0700

    Fixing Admin Seeder

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,7 +8,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        return redirect()->route('dashboard');
     }
 
     public function roleRequest()
